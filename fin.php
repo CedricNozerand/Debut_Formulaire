6a1c26f507d2ea9e18f902df3ed6e564dd652fbe
CedricNozerand <43014857+CedricNozerand@users.noreply.github.com>
2020-04-16 15:26:48 +0200

    Update fin.php

--- fin.php
+++ fin.php
@@ -2,4 +2,8 @@
 	echo "Nom: ".$_POST['user_name']."</br>";
 	echo "Prenom: ".$_POST['user_prenom']."</br>";
 	echo "Mail: ".$_POST['user_mail']."</br>";
+
+	if(!empty($_POST['user_name']) && !empty($_POST['user_prenom']) && !empty($_POST['user_mail'])){
+		
+	}
 ?>
